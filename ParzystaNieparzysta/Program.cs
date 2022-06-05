706bcd1fc5ef923590f5bdbaab7571e9faa9c1d1
Charlie_MT <pavvelzawadzki@gmail.com>
2022-06-05 12:54:36 +0200

    first program writen, ruuning, no errors

--- ParzystaNieparzysta/Program.cs
+++ ParzystaNieparzysta/Program.cs
@@ -10,6 +10,57 @@ namespace ParzystaNieparzysta
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Witaj w programie gdzie sprawdzimy czy liczba jest parzysta lub nie");
+
+            while (true)
+            {
+
+                var number = DataCheck();
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Twoja liczba jest parzysta");
+                }
+                else
+                {
+                    Console.WriteLine("Twoja liczba jest nieparzysta");
+                }
+
+                if (ExitMenu())
+                    break;
+
+            }
+            Console.ReadLine();
         }
+
+        private static int DataCheck()
+        {
+            while (true)
+            {
+                Console.WriteLine("Proszę podać liczbę :");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podano nie prawidłową wartoś\n Sprobuj ponownie");
+                    continue;
+                }
+
+                return number;
+            }
+        }
+
+        private static bool ExitMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Aby zakonczyc nacisniej X, aby kontynuowac C");
+                var choose = Console.ReadLine().ToUpper();
+
+                if (choose.Equals("X"))
+                    return true;
+
+                if (choose.Equals("C"))
+                    return false;
+            }
+        }
+
     }
 }
